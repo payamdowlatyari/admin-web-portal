1966af4740e2d503f115e1e227be100e0a137540
Payam Dowlatyari <pdowlatyari@gmail.com>
2020-04-03 18:40:53 -0700

    add charts

--- src/App.js
+++ src/App.js
@@ -4,32 +4,81 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Toolbar from './components/Toolbar/Toolbar';
 import './App.css';
 import Footbar from './components/Footbar/Footbar';
+import BarChart from './components/Charts/BarChart';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     sidebarOpen: true
-  //   };
-  //   this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  // }
- 
-  // onSetSidebarOpen(open) {
-  //   this.setState({ sidebarOpen: open });
-  // }
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Los Angeles', 'San Diego', 'San Francisco', 'San Jose', 'Sacramento', 'Irvine'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
   render() {
+
   return (
     <div className="App">
       <div className="container">
      <Toolbar />
     
     </div>
+    <div>
     <Sidebar pageWrapId={"page-wrap"} outerContainerId={"App"} />     
-<main></main>
-<footer>
+    <div className="container" style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+              <BarChart chartData={this.state.chartData} location="California" legendPosition="bottom"/>
+
+              </div>
+<div className="container" style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+<footer className="navbar-fixed-bottom">
+
 <Footbar/>
 
 </footer>
+</div>
+    </div>
+
     </div>
   );
   }
--- src/components/Footbar/Footbar.js
+++ src/components/Footbar/Footbar.js
@@ -1,39 +1,15 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import './Footbar.css';
 
 const Footbar = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
-           
-          </MDBCol>
-          <MDBCol md="6">
-            {/* <ul>
-              <li className="list-unstyled">
-                <a href="#!">Link 1</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 2</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
-              </li>
-            </ul> */}
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+
       <div className="footer-copyright text-center py-3">
-        <MDBContainer>
-          &copy; {new Date().getFullYear()} Copyright
-        </MDBContainer>
+        CalPlug &copy; {new Date().getFullYear()} All Rights Reserved
+ 
+
       </div>
-    </MDBFooter>
-    
+
   );
 }
 
